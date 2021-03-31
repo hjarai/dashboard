26fe027802c2779593a77c782f73017a552051ce
hjarai <harai@middlebury.edu>
2021-03-30 21:22:13 -0400

    edited dashboard for senti data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react'
 import ChartistGraph from 'react-chartist'
+import data_json from '../data/filtered.json'
+
+const data = data_json;
 
 class Dashboard extends Component {
+
   render() {
-    let dataPie = {
-      labels: ["40%", "20%", "40%"],
-      series: [40, 20, 40]
-    }
-    let dataSales = {
-      labels: [
-        "9:00AM",
-        "12:00AM",
-        "3:00PM",
-        "6:00PM",
-        "9:00PM",
-        "12:00PM",
-        "3:00AM",
-        "6:00AM"
-      ],
-      series: [
-        [287, 385, 490, 492, 554, 586, 698, 695],
-        [67, 152, 143, 240, 287, 335, 435, 437],
-        [23, 113, 67, 108, 190, 239, 307, 308]
-      ]
+    // let dataPie = {
+    //   labels: ["40%", "20%", "40%"],
+    //   series: [40, 20, 40]
+    // }
+    // let dataSales = {
+    //   labels: [
+    //     "9:00AM",
+    //     "12:00AM",
+    //     "3:00PM",
+    //     "6:00PM",
+    //     "9:00PM",
+    //     "12:00PM",
+    //     "3:00AM",
+    //     "6:00AM"
+    //   ],
+    //   series: [
+    //     [287, 385, 490, 492, 554, 586, 698, 695],
+    //     [67, 152, 143, 240, 287, 335, 435, 437],
+    //     [23, 113, 67, 108, 190, 239, 307, 308]
+    //   ]
+    // }
+    let dataLine = {
+      labels: ["Thurs", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
+      series: [Object.values(data.Senti_Sum), Object.values(data.Tensi_Sum)]
     }
     return (
       <div className="content">
         <div className="container-fluid">
           <div className="row">
 
-            <div className="col-md-4">
+            {/* <div className="col-md-4">
               <div className="card ">
                 <div className="card-header ">
                   <h4 className="card-title">Email Statistics</h4>
@@ -48,8 +56,8 @@ class Dashboard extends Component {
                                     </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-8">
+            </div> */}
+            {/* <div className="col-md-8">
               <div className="card">
                 <div className="card-header ">
                   <h4 className="card-title">Users Behavior</h4>
@@ -63,6 +71,27 @@ class Dashboard extends Component {
                     <i className="fa fa-circle text-info"></i> Open
                     <i className="fa fa-circle text-danger"></i> Click
                     <i className="fa fa-circle text-warning"></i> Click Second Time
+                </div>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-history"></i> Updated 3 minutes ago
+                  </div>
+                </div>
+              </div>
+            </div> */}
+            <div className="col-md-8">
+              <div className="card">
+                <div className="card-header ">
+                  <h4 className="card-title">Twitter Analysis</h4>
+                  <p className="card-category">1 week tracking</p>
+                </div>
+                <div className="card-body ">
+                  <ChartistGraph data={dataLine} type="Line" />
+                </div>
+                <div className="card-footer ">
+                  <div className="legend">
+                    <i className="fa fa-circle text-info"></i> Happiness/Sadness
+                    <i className="fa fa-circle text-danger"></i> Relax/Stress
                 </div>
                   <hr />
                   <div className="stats">
