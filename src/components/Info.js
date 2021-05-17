1f691efc7fa792f09fc7fb72608a90f8c1829197
hjarai <harai@middlebury.edu>
2021-05-17 14:55:31 -0400

    FINAL

--- src/components/Info.js
+++ src/components/Info.js
@@ -13,27 +13,47 @@ class Info extends Component{
           <div className="col-md-8">
             <div className="card">
               <div className="card-header ">
-                <h4 className="card-title">About MiddMood</h4>
+                <h4 className="card-title">Our App</h4>
                 <p className="card-category">What is this dashboard?</p>
               </div>
               <div className="card-body ">
-                <p>MiddMood, a sentiment-analysis display based upon Middlebury student Twitter content and newspaper publications. As COVID-19 safety restrictions remain in effect on campus, the opportunity for social connection among students continue to be scarce. Our project MiddMood addresses the absence of social solidarity in the Middlebury student body, by quantifying the answer the question, "How is the Midd student body feeling today?"
+                <p>MiddMood is a sentiment-analysis display based upon Middlebury student Twitter content and newspaper publications. We believe that our project addresses the absence of social solidarity in the Middlebury student body, by quantitatively answering the question, "How is the Midd student body feeling today?" 
 
-Using student Tweets and campus newspaper publications as data source, we will conduct frequency and sentiment analyses to extract topical keywords and determine the level of ambient happiness and stress. By visualizing these findings on a dashboard, students will be able to check in on not only the trends and mood of each week, but how they've changed over the course of the semester.
+Using student Tweets and campus newspaper publications as data sources, we conduct word-frequency and sentiment analysis to extract topical keywords and determine the level of ambient happiness in our community.
 
-In this particularly isolating semester, we hope MiddMood can be the catalyst for students finding empathy and feeling a stronger connection to the rest of the school.</p>
+By visualizing these findings on a dashboard, students can check in on the trends and mood of each week, and how they've changed over the course of the semester. In this particularly isolating year, we hope MiddMood can be the catalyst for students finding empathy and feeling a stronger connection to the rest of the school.
+
+</p>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-header ">
+                <h4 className="card-title">Our App</h4>
+                <p className="card-category">What if I don’t agree with the mood dial?
+</p>
+              </div>
+              <div className="card-body ">
+                <p>MWe’d like to emphasize that our dashboard should in no way suggest how you should feel. Rather, our aim is to provide an assessment of ambient mood from a collection of student-written text; this may or may not be consistent with your feelings today.
+
+Lastly, if you think that our assessment is incorrect, please Tweet at us (@MiddMood) and say that we are wrong! We’ll add you to our model so your content can be incorporated into the dashboard!
+
+
+</p>
               </div>
             </div>
             
             <div className="card">
               <div className="card-header ">
-                <h4 className="card-title">About Us</h4>
+                <h4 className="card-title">Who are We?</h4>
                 <p className="card-category">Who are behind MiddMood?</p>
               </div>
               <div className="card-body ">
                 <img src={hirona} alt="hirona" width="200" height="200" />
                 <img src={culton} alt="culton" width="200" height="200" />
-                <p>Hirona ('22) and Culton ('21.5) are Middlebbury CS seniors. This project is part of CS701 Senior Seminar with Prof. Vaccari.</p>
+                <p>Hirona ('22) and Culton ('21.5) are Middlebury Computer Science students enrolled in the CS701 Senior Seminar with Prof. Vaccari—MiddMood is our final project!
+
+</p>
 
               </div>
             </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,18 +13,18 @@ class Layout extends Component {
     this.state ={
       range: 'week',
       data: [],
+      last: "today"
     };
   };
 
   componentDidMount() {
-    // fetch('last_one.txt')
-    fetch('https://www.cs.middlebury.edu/~ckoster/data/fur_hirona.txt')
+    fetch(`/~ckoster/data/fur_hirona.txt`)
         .then(res => res.text())
         .then(body => {
             const lines = body.split("\n");
             const data_array = lines.filter(line=>line!=="").map((line) => JSON.parse(line))
-            // try catch 
             this.setState({data:data_array});
+            this.setState({last:data_array.slice(this.state.data.length-1)[0].date})
         });
   }
 
@@ -35,9 +35,11 @@ class Layout extends Component {
 
     const filtered_data = (period) => {
       if (period === 'week'){
-        return this.state.data.slice(data_length-7, data_length-1);
+        return this.state.data.slice(data_length-7, data_length);
       } else if (period ==='month'){
-        return this.state.data.slice(data_length-30, data_length-1);
+        return this.state.data.slice(data_length-30, data_length);
+      } else if (period ==='semester'){
+        return this.state.data.slice(data_length-90, data_length);
       } else {
         return this.state.data
       }
@@ -48,10 +50,11 @@ class Layout extends Component {
 
     return (
       <div className="content">
-        <div className="container-fluid">
+        <div className="cardoutside-fluid">
+
           <div className="row">
 
-            <div className="col-md-8">
+            <div className="col-md-12">
               <div className="card">
                 
                 <div className="rowC">
@@ -62,7 +65,8 @@ class Layout extends Component {
                   <button onClick={() => this.setState({range: "week"})}>This week</button>
                   <button onClick={() => this.setState({range: "month"})}>This Month</button>
                   <button onClick={() => this.setState({range: "semester"})}>This Semester</button>
-                  <Test2 data={filtered_data(this.state.range)}/>
+                  <button onClick={() => this.setState({range: "2020"})}>Since 2020</button>
+                  <Test2 data={filtered_data(this.state.range)} last={this.state.last}/>
                 </div>
               </div>
             </div>
